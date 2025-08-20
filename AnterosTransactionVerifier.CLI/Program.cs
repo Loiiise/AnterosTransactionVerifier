ae76c545eaf24a0c25354a8dbe5f4038d6b607fa
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-08-20 19:42:14 +0200

    setup initial matching flow

--- AnterosTransactionVerifier.CLI/Program.cs
+++ AnterosTransactionVerifier.CLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AnterosTransactionVerifier.CLI;
+using AnterosTransactionVerifier.Logic;
 using AnterosTransactionVerifier.Services.FileHandling;
 using AnterosTransactionVerifier.Services.TransactionConversion.Stringification;
 using AnterosTransactionVerifier.Services.TransactionWriting;
@@ -19,6 +20,47 @@ var transactionWriter = new FileTransactionWriter(
 var bank = new TransactionHandler(configuration.Bank);
 var bookkeeping = new TransactionHandler(configuration.Bookkeeping);
 
+bank.GenerateDictionairy();
+bookkeeping.GenerateDictionairy();
 
+WriteAnalysisLine(bank.Transactions, bank.Name);
+WriteAnalysisLine(bookkeeping.Transactions, bookkeeping.Name);
 
+var unmatchedBankTransactions = new List<Transaction>();
+var unmatchedBookkeepingTransactions = new List<Transaction>();
+
+foreach (var amount in bank.TransactionByAmount.Keys)
+{
+    var bankTransactions = bank.TransactionByAmount.GetValueOrDefault(amount) ?? new List<Transaction>();
+    var bookkeepingTransactions = bookkeeping.TransactionByAmount.GetValueOrDefault(amount) ?? new List<Transaction>();
+
+    if (bankTransactions.Count == bookkeepingTransactions.Count) continue;
+
+    unmatchedBankTransactions.AddRange(bankTransactions);
+}
+
+foreach (var amount in bookkeeping.TransactionByAmount.Keys)
+{
+    var bookkeepingTransactions = bookkeeping.TransactionByAmount.GetValueOrDefault(amount) ?? new List<Transaction>();
+    var bankTransactions = bank.TransactionByAmount.GetValueOrDefault(amount) ?? new List<Transaction>();
+
+    if (bankTransactions.Count == bookkeepingTransactions.Count) continue;
+
+    unmatchedBookkeepingTransactions.AddRange(bookkeepingTransactions);
+}
+
+if (!unmatchedBankTransactions.Any() ||
+    !unmatchedBookkeepingTransactions.Any())
+{
+    Console.WriteLine("All transactions match!");
+    return;
+}
+
+Console.WriteLine("Unable to match all transactions one on one. Attempting multiples matching...");
+WriteAnalysisLine(unmatchedBankTransactions, "unmatched " + bank.Name);
+WriteAnalysisLine(unmatchedBookkeepingTransactions, "unmatched " + bookkeeping.Name);
+
+// Todo: match batches manually. Needed in datatypes for that
+
+void WriteAnalysisLine(List<Transaction> transactions, string name) => Console.WriteLine($"Analysing {transactions.Count} {name} transactions with a total value of {transactions.Sum(t => t.Amount)} euros");
 
--- AnterosTransactionVerifier.CLI/TransactionHandler.cs
+++ AnterosTransactionVerifier.CLI/TransactionHandler.cs
@@ -6,6 +6,8 @@ using AnterosTransactionVerifier.Services.TransactionReading;
 namespace AnterosTransactionVerifier.CLI;
 internal class TransactionHandler
 {
+    public List<Transaction> Transactions { get; set; } = new();
+    public Dictionary<decimal, List<Transaction>> TransactionByAmount { get; private set; } = new();
     private readonly TransactionConfiguration _transactionConfiguration;
     private readonly ITransactionReader _transactionReader;
 
@@ -16,4 +18,14 @@ internal class TransactionHandler
             new BasicFileReader(transactionConfiguration.TransactionFileLocation, transactionConfiguration.SkipFirstLine),
             new TransactionParser(transactionConfiguration.ToParserConfiguration()));
     }
+
+    internal void GenerateDictionairy()
+    {
+        Transactions = _transactionReader.ReadAllTransactions().ToList();
+        TransactionByAmount = Transactions
+            .GroupBy(t => t.Amount)
+            .ToDictionary(g => g.Key, g => g.ToList());
+    }
+
+    public string AnalysisLine() => $"Analysing {Transactions.Count} bank transactions with a total value of {Transactions.Sum(t => t.Amount)} euros";
 }
